466bea5ecd19bf944b693b868e4779e43ec274ef
Nícholas André <nicholas.andre@10up.com>
2021-09-23 20:22:51 -0300

    feat: scaffold next.js app

---
+++ scaffold/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+// eslint-disable-next-line react/prop-types
+const MyApp = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
+
+export default MyApp;
