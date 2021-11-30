bc192c918a6ca3eda5f43ae76fdf986be936291a
a-lagun <antonio.laguna@10up.com>
2021-11-30 15:23:09 +0100

    Adding useEffectAfterRender hook
    #14

---
+++ packages/hooks/src/useEffectAfterRender/__tests__/dom.ts
@@ -0,0 +1,26 @@
+import { renderHook } from '@testing-library/react-hooks/dom';
+import { useEffectAfterRender } from '../..';
+
+describe('useEffectAfterRender', () => {
+	it('should be defined', () => {
+		expect(useEffectAfterRender).toBeDefined();
+	});
+
+	it('should render', () => {
+		const { result } = renderHook(() => useEffectAfterRender(() => {}));
+		expect(result.error).toBeUndefined();
+	});
+
+	it('should not call after mount but after render', () => {
+		const cb = jest.fn();
+		const { rerender } = renderHook(({ value }) => useEffectAfterRender(cb, [value]), {
+			initialProps: {
+				value: 1,
+			},
+		});
+		expect(cb).not.toHaveBeenCalled();
+
+		rerender({ value: 10 });
+		expect(cb).toHaveBeenCalled();
+	});
+});
---
+++ packages/hooks/src/useEffectAfterRender/__tests__/ssr.ts
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react-hooks/server';
+import { useEffectAfterRender } from '../..';
+
+describe('useEffectAfterRender', () => {
+	it('should be defined', () => {
+		expect(useEffectAfterRender).toBeDefined();
+	});
+
+	it('should render', () => {
+		const { result } = renderHook(() => useEffectAfterRender(() => {}));
+		expect(result.error).toBeUndefined();
+	});
+});
