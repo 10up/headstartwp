55402c4be19e9f95e3b1439d64d84b60c1edb8b7
Nícholas Oliveira <nicholas.andre@10up.com>
2021-12-27 12:41:25 -0300

    feat(gutenberg): automatically specifying the image domain based on WP URL

---
+++ packages/next/src/utils/withHeadlessConfig.ts
@@ -0,0 +1,36 @@
+import { NextConfig } from 'next';
+
+/**
+ * HOC used to wrap the nextjs config object with the headless config object.
+ *
+ * @param {object} nextConfig The nextjs config object
+ * @returns
+ */
+export function withHeadlessConfig(nextConfig: NextConfig = {}, headlessConfig = {}): NextConfig {
+	const imageDomains: Array<string> = [];
+
+	try {
+		const imageMainDomain = new URL(process.env.NEXT_PUBLIC_HEADLESS_WP_URL || '');
+
+		imageDomains.push(imageMainDomain.hostname);
+	} catch (e) {
+		// do nothing
+	}
+
+	return {
+		...nextConfig,
+		images: {
+			domains: imageDomains,
+		},
+		webpack: (config, { webpack }) => {
+			config.plugins.push(
+				new webpack.DefinePlugin({
+					HEADLESS_CONFIG: webpack.DefinePlugin.runtimeValue(function () {
+						return JSON.stringify(headlessConfig);
+					}),
+				}),
+			);
+			return config;
+		},
+	};
+}
--- projects/wp-nextjs/next.config.js
+++ projects/wp-nextjs/next.config.js
@@ -1,7 +1,10 @@
-const webpack = require('webpack'); //eslint-disable-line
-const fs = require('fs');
+const fs = require('fs'); // eslint-disable-line
+const path = require('path'); // eslint-disable-line
 
-const headlessConfigPath = './headless.config.js';
+// eslint-disable-next-line
+const { withHeadlessConfig } = require('@10up/headless-next/utils');
+
+const headlessConfigPath = path.join(process.cwd(), 'headless.config.js');
 
 // the headless config is an empty object by default
 let headlessConfig = {};
@@ -11,37 +14,10 @@ if (fs.existsSync(headlessConfigPath)) {
 }
 
 /**
- * HOC used to wrap the nextjs config object with the headless config object.
- *u
- *
- * @param {object} nextConfig The nextjs config object
- * @returns
- */
-const withHeadlessConfig = (nextConfig = {}) => {
-	return {
-		...nextConfig,
-		webpack: (config) => {
-			config.plugins.push(
-				new webpack.DefinePlugin({
-					HEADLESS_CONFIG: webpack.DefinePlugin.runtimeValue(function () {
-						return JSON.stringify(headlessConfig);
-					}),
-				}),
-			);
-			return config;
-		},
-	};
-};
-
-/**
  * Update whatever you need within the nextConfig object.
  *
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-	images: {
-		domains: ['elasticpress.test', 'js1.10up.com'],
-	},
-};
+const nextConfig = {};
 
-module.exports = withHeadlessConfig(nextConfig);
+module.exports = withHeadlessConfig(nextConfig, headlessConfig);
