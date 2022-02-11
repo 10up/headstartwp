c77f6c3fd48718317f773d0c33b493a610327d17
Nícholas André <nicholas.andre@10up.com>
2022-02-11 11:29:23 -0300

    feat(preview): moving preview api handler to next package

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,3 +1,4 @@
 export * from './data';
 export * from './components';
 export * from './utils';
+export * from './handlers';
--- projects/wp-nextjs/src/pages/api/preview.js
+++ projects/wp-nextjs/src/pages/api/preview.js
@@ -1,43 +1,5 @@
-import { getCustomPostType } from '@10up/headless-core/utils';
-import { fetchHookData } from '@10up/headless-next/data';
+import { previewHandler } from '@10up/headless-next';
 
 export default async function handler(req, res) {
-	const { post_id, post_type, is_revision, token } = req.query;
-	const revision = is_revision === '1';
-	const { data } = await fetchHookData(
-		'usePost',
-		{
-			params: { path: [] },
-		},
-		{
-			params: {
-				id: post_id,
-				postType: post_type,
-				revision,
-				authToken: token,
-			},
-		},
-	);
-
-	const id = Number(post_id);
-
-	const result = Array.isArray(data?.result) ? data.result[0] : data.result;
-	if (result?.id === id || result?.parent === id) {
-		const { slug } = result;
-		res.setPreviewData({
-			id,
-			postType: post_type,
-			revision,
-			authToken: token,
-		});
-
-		const postTypeDef = getCustomPostType(post_type);
-		const singleRoute = postTypeDef.single || '/';
-		const prefixRoute = singleRoute === '/' ? '' : singleRoute;
-		const slugOrId = revision ? post_id : slug || post_id;
-
-		return res.redirect(`${prefixRoute}/${slugOrId}-preview=true`);
-	}
-
-	return res.end('preview mode not enabled');
+	return previewHandler(req, res);
 }
