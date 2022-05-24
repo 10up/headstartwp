6fbd195b47ccd89c0a261957af58fb1061e6a439
Nícholas André <nicholas.andre@10up.com>
2022-05-24 15:30:11 -0300

    docs: removing old docs

--- packages/core/docs/components/index.md
+++
@@ -1,8 +0,0 @@
-## Components
-
-`@10up/headless-core` ships a set of components for WordPress Headless implementations.
-
-### Table of Concents
- - [SettingsProvider](./SettingsProvider.md)
- - [BlocksRenderer](./BlocksRenderer.md)
- - [Menu](./Menu.md)
--- packages/core/src/react/blocks/ButtonBlock.tsx
+++ packages/core/src/react/blocks/ButtonBlock.tsx
@@ -6,23 +6,89 @@ import { Align, Border, Colors, IBlockAttributes, Typography } from './types';
 import { useBlock } from './hooks';
 import { useBlockAttributes } from './hooks/useBlockAttributes';
 
+/**
+ * The interface for components rendered by {@link ButtonBlock}
+ */
 export interface ButtonBlockProps extends IBlockAttributes {
+	/**
+	 * The button url
+	 */
 	url?: string;
+
+	/**
+	 * The title of the link
+	 */
 	title?: string;
+
+	/**
+	 * The button text
+	 */
 	text?: string;
+
+	/**
+	 * The link target
+	 */
 	linkTarget?: string;
+
+	/**
+	 * The link rel
+	 */
 	rel?: string;
+
+	/**
+	 * The colors object
+	 */
 	colors?: Colors;
+
+	/**
+	 * The link placeholer text
+	 */
 	placeholder?: string;
+
+	/**
+	 * The border object
+	 */
 	border?: Border;
+
+	/**
+	 * The block style
+	 */
 	blockStyle?: string;
+
+	/**
+	 * The typography object
+	 */
 	typography?: Typography;
+
+	/**
+	 * The align value
+	 */
 	align: Align;
+
+	/**
+	 * The width value
+	 */
 	width?: string;
 }
 
 export interface IButtonBlock extends IBlock<ButtonBlockProps> {}
 
+/**
+ * The ButtonBlock component implements block parsing for the Button block.
+ *
+ * This component must be used within a {@link BlocksRenderer} component.
+ *
+ * ```tsx
+ * <BlocksRenderer html={html}>
+ * 	<ButtonBlock component={DebugComponent} />
+ * </BlocksRenderer>
+ * ```
+ *
+ * @category Blocks
+ *
+ * @param props Component properties
+ *
+ */
 export function ButtonBlock({ domNode: node, children, component: Component }: IButtonBlock) {
 	const { className, name } = useBlock(node);
 	const { align, blockStyle, border, colors, typography, width } = useBlockAttributes(node);
