2e575bae722be54159a7626b37cdc266250922f1
Nícholas André <nicholas.andre@10up.com>
2022-05-30 14:20:17 -0300

    feat: new docs site powered by express

---
+++ projects/docs/src/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const Redis = require('ioredis');
+const RedisStore = require('connect-redis')(session);
+
+const authRouter = require('./routes/auth');
+
+const redisClient = new Redis(
+	`rediss://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(
+	session({
+		store: new RedisStore({ client: redisClient }),
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false,
+	}),
+);
+app.use(passport.authenticate('session'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', authRouter);
+app.use('/', passport.authenticate('session'));
+app.use('/', (req, res, next) => {
+	if (req.user) {
+		return express.static(path.join(__dirname, '../public'))(req, res, next);
+	}
+	return res.render('login');
+});
+app.listen(port);
+
+console.log(`Server started at http://localhost:${port}`);
