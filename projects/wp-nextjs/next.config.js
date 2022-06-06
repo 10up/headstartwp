e3f00d82293911f95f21ff9a583338d5e457c3f4
Nícholas André <nicholas.andre@10up.com>
2022-06-06 11:19:00 -0300

    feat: more linaria integration into the framework

--- projects/wp-nextjs/next.config.js
+++ projects/wp-nextjs/next.config.js
@@ -6,38 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const headlessConfig = require('./headless.config');
 
-const LINARIA_EXTENSION = '.linaria.module.css';
-
-function traverse(rules) {
-	for (const rule of rules) {
-		if (typeof rule.loader === 'string' && rule.loader.includes('css-loader')) {
-			if (
-				rule.options &&
-				rule.options.modules &&
-				typeof rule.options.modules.getLocalIdent === 'function'
-			) {
-				const nextGetLocalIdent = rule.options.modules.getLocalIdent;
-				rule.options.modules.mode = 'local';
-				rule.options.modules.auto = true;
-				rule.options.modules.exportGlobals = true;
-				rule.options.modules.exportOnlyLocals = false;
-				rule.options.modules.getLocalIdent = (context, _, exportName, options) => {
-					if (context.resourcePath.includes(LINARIA_EXTENSION)) {
-						return exportName;
-					}
-					return nextGetLocalIdent(context, _, exportName, options);
-				};
-			}
-		}
-		if (typeof rule.use === 'object') {
-			traverse(Array.isArray(rule.use) ? rule.use : [rule.use]);
-		}
-		if (Array.isArray(rule.oneOf)) {
-			traverse(rule.oneOf);
-		}
-	}
-}
-
 /**
  * Update whatever you need within the nextConfig object.
  *
@@ -46,25 +14,6 @@ function traverse(rules) {
 const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
-	},
-	webpack(config) {
-		traverse(config.module.rules);
-		config.module.rules.push({
-			test: /\.(tsx|ts|js|mjs|jsx)$/,
-			exclude: /node_modules/,
-			use: [
-				{
-					loader: require.resolve('@linaria/webpack-loader'),
-					options: {
-						sourceMap: process.env.NODE_ENV !== 'production',
-						...(nextConfig.linaria || {}),
-						extension: LINARIA_EXTENSION,
-					},
-				},
-			],
-		});
-
-		return config;
 	},
 };
 
