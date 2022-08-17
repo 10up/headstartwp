152ffc93ef97ab94ad7f3dca407343c2f5c065ab
Luiz Cláudio <luizcns18@gmail.com>
2022-08-17 17:13:44 -0300

    fix: remove useless log

--- packages/next/src/middlewares/appMidleware.ts
+++ packages/next/src/middlewares/appMidleware.ts
@@ -9,7 +9,6 @@ function isStaticAssetRequest(req: NextRequest) {
 
 export async function AppMiddleware(req: NextRequest) {
 	const { redirectStrategy } = getHeadlessConfig();
-	console.log({ redirectStrategy });
 
 	if (isStaticAssetRequest(req)) {
 		return NextResponse.next();
