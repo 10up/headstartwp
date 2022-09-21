d41a49ab06ef915055a77449051e6534839a9939
Nícholas Oliveira <nicholas.andre@10up.com>
2022-09-21 14:08:14 -0300

    feat: optimize addHookData

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: 'ts-jest/presets/js-with-ts',
+	transform: {
+		'\\.[jt]sx?$': ['ts-jest', { preset: 'ts-jest/presets/js-with-ts' }],
+	},
 	testEnvironment: 'jsdom',
 	testPathIgnorePatterns: ['dist'],
 };
--- packages/next/jest.config.js
+++ packages/next/jest.config.js
@@ -2,10 +2,5 @@ const config = require('../../jest.config');
 
 module.exports = {
 	...config,
-	globals: {
-		'ts-jest': {
-			tsconfig: 'tsconfig-cjs.json',
-		},
-	},
 	setupFilesAfterEnv: ['./jest.setup.ts'],
 };
