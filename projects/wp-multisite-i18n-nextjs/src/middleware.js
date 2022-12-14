0a8d01df5bc08a01a8c491bb753807017efa35d7
Nícholas Oliveira <nicholas.andre@10up.com>
2022-12-14 14:52:02 -0300

    feat: update projects to use config matcher

--- projects/wp-multisite-i18n-nextjs/src/middleware.js
+++ projects/wp-multisite-i18n-nextjs/src/middleware.js
@@ -2,17 +2,21 @@ import { AppMiddleware } from '@10up/headless-next/middlewares';
 
 import { NextResponse } from 'next/server';
 
-const PUBLIC_FILE = /\.(.*)$/;
+export const config = {
+	matcher: [
+		/*
+		 * Match all paths except for:
+		 * 1. /api routes
+		 * 2. /_next (Next.js internals)
+		 * 3. /fonts (inside /public)
+		 * 4. all root files inside /public (e.g. /favicon.ico)
+		 */
+		'/((?!api|_next|fonts[\\w-]+\\.\\w+).*)',
+	],
+};
 
 export async function middleware(...args) {
-	const req = args[0];
-	if (
-		req.nextUrl.pathname.startsWith('/_next') ||
-		req.nextUrl.pathname.includes('/api/') ||
-		PUBLIC_FILE.test(req.nextUrl.pathname)
-	) {
-		return NextResponse.next();
-	}
+	const [req] = args;
 
 	if (req.nextUrl.locale === 'default') {
 		return NextResponse.redirect(new URL(`/en${req.nextUrl.pathname}`, req.url));
