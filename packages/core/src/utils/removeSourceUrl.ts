e0dfa7781153e38c2fa134209263f4a6405e1023
Nícholas André <nicholasio.oliveira@gmail.com>
2022-12-21 08:54:13 -0300

    feat: warn if values are undefined (#287)

--- packages/core/src/utils/removeSourceUrl.ts
+++ packages/core/src/utils/removeSourceUrl.ts
@@ -1,3 +1,5 @@
+import { warn } from './errors';
+
 export type removeSourceUrlType = {
 	/**
 	 * The link url, possibly with the source url.
@@ -26,6 +28,16 @@ export type removeSourceUrlType = {
  * @returns The URL without the Source URL.
  */
 export function removeSourceUrl({ link, backendUrl, publicUrl = '/' }: removeSourceUrlType) {
+	if (typeof link === 'undefined') {
+		warn('link is undefined, double check if you are passing a valid value');
+		return '';
+	}
+
+	if (typeof backendUrl === 'undefined') {
+		warn('backendUrl is undefined, double check if you are passing a valid value');
+		return link;
+	}
+
 	// Ensure `sourceUrl` and `publicUrl` always include a trailing slash. All
 	// the logic below is based on those variables fulfilling that condition.
 	const sourceUrl = backendUrl.replace(/\/?$/, '/');
