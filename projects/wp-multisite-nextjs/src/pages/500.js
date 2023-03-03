028e8ece7c3efdcac2d04c718e6fc01e9e7c7641
Nícholas Oliveira <nicholas.andre@10up.com>
2023-03-03 14:30:11 -0300

    fix: 404 and 500 on multisite

--- projects/wp-multisite-nextjs/src/pages/500.js
+++ projects/wp-multisite-nextjs/src/pages/500.js
@@ -1,32 +1,14 @@
-import { addHookData, fetchHookData, handleError, useAppSettings } from '@10up/headless-next';
 import Head from 'next/head';
-import { resolveBatch } from '../utils/promises';
 
 const ServerErrorPage = () => {
-	const { data } = useAppSettings();
-
 	return (
 		<>
 			<Head>
-				<title>Error - {data.settings.site_name} </title>
-				<meta name="description" content={data.settings.site_desc} />
+				<title>Error</title>
 			</Head>
 			<h1>500 - Internal Server Error</h1>
 		</>
 	);
 };
 
-export async function getStaticProps(context) {
-	try {
-		// fetch batch of promises and throws errors selectively
-		// passing `throw:false` will prevent errors from being thrown for that promise
-		const settledPromises = await resolveBatch([
-			{ func: fetchHookData(useAppSettings.fetcher(), context), throw: false },
-		]);
-
-		return addHookData(settledPromises, { revalidate: 60 });
-	} catch (e) {
-		return handleError(e, context);
-	}
-}
 export default ServerErrorPage;
