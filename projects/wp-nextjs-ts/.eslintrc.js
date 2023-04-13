cacdbf1169dc12711316e3f352a1b1dd0c3b3a12
Nícholas Oliveira <nicholas.andre@10up.com>
2023-04-12 22:09:15 -0300

    chore: improved compatibility

--- projects/wp-nextjs-ts/.eslintrc.js
+++ projects/wp-nextjs-ts/.eslintrc.js
@@ -6,12 +6,6 @@ module.exports = {
 		jsdoc: {
 			mode: 'typescript',
 		},
-		'import/core-modules': [
-			'@10up/headless-core/react',
-			'@10up/headless-core/utils',
-			'@10up/headless-next/config',
-			'@10up/headless-next/middlewares',
-		],
 	},
 	rules: {
 		'import/extensions': [
