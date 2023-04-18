03dc5e869a7db84c07e067f6f45815ed274adedd
Nícholas Oliveira <nicholas.andre@10up.com>
2023-04-18 01:59:29 -0300

    fix: build

--- projects/wp-nextjs-ts/src/components/PageContent.tsx
+++ projects/wp-nextjs-ts/src/components/PageContent.tsx
@@ -3,7 +3,8 @@ import { PostParams } from '@10up/headless-core';
 import { FC, ReactElement } from 'react';
 import dynamic from 'next/dynamic';
 
-const Blocks = dynamic(() => import('./Blocks'));
+// eslint-disable-next-line import/no-unresolved
+const Blocks = dynamic(() => import('./Blocks.js').then((mod) => mod.default));
 
 /**
  * This is an example of how an inner component can access the data without explicitly passing the data to it.
