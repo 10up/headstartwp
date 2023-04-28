823d520cd9af3e61f4ca0d65c66ae0017de24390
Nícholas André <nicholasio.oliveira@gmail.com>
2023-04-28 01:25:38 -0300

    fix: remove ref to old packages (#415)

--- packages/next/src/config/withHeadlessConfig.ts
+++ packages/next/src/config/withHeadlessConfig.ts
@@ -162,9 +162,6 @@ export function withHeadlessConfig(
 						/packages\/next\/config\/loader/.test(source) ||
 						/packages\/core/.test(source) ||
 						// for the pubished packaged version
-						/@10up\/headless-next\/config\/loader/.test(source) ||
-						/@10up\/headless-core/.test(source) ||
-						// for the new name
 						/@headstartwp\/next\/config\/loader/.test(source) ||
 						/@headstartwp\/core/.test(source)
 					) {
