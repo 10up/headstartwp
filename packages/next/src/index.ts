1cc4154a25cde6ba009dc08ce972ecd1a9d62b36
Nícholas André <nicholas.andre@10up.com>
2023-05-10 22:43:40 +0000

    fix: re-add workaorund

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -3,6 +3,16 @@
  *
  * @packageDocumentation
  */
+import { setHeadstartWPConfig } from '@headstartwp/core';
+import getConfig from 'next/config';
+
+if (typeof window === 'undefined') {
+	const { serverRuntimeConfig } = getConfig();
+
+	if (serverRuntimeConfig && serverRuntimeConfig.headlessConfig) {
+		setHeadstartWPConfig(serverRuntimeConfig.headlessConfig);
+	}
+}
 
 export * from './data/index';
 export * from './components/index';
