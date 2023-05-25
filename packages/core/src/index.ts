723ab0c232b17ea7fc1407a3e7fa5f0515a21ac6
Nícholas Oliveira <nicholas.andre@10up.com>
2023-05-25 10:56:18 -0300

    feat: re-export html-react-parser

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,5 @@ export * from './data';
 export * from './utils';
 export * from './dom';
 export * from './types';
+export * from 'html-react-parser';
+export { default as HTMLReactParser } from 'html-react-parser';
