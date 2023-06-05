f9f355895c430ceddf5b127193a50170ce3cfe9a
Nícholas Oliveira <nicholas.andre@10up.com>
2023-06-05 12:15:55 -0300

    fix: improved logic for determining the post path for revalidation

--- packages/next/src/handlers/revalidateHandler.ts
+++ packages/next/src/handlers/revalidateHandler.ts
@@ -43,14 +43,9 @@ export async function revalidateHandler(req: NextApiRequest, res: NextApiRespons
 	}
 
 	const host = req.headers.host ?? '';
-	console.log('HOST', host);
 	const site = getSiteByHost(host, typeof locale === 'string' ? locale : undefined);
-	console.log('SITE', site);
 	const isMultisiteRequest = site !== null && typeof site.sourceUrl === 'string';
-	console.log('isMultisiteRequest', isMultisiteRequest);
 	const { sourceUrl } = isMultisiteRequest ? site : getHeadlessConfig();
-	console.log('source', sourceUrl);
-	console.log('getHeadlessConfig', getHeadlessConfig());
 	// call WordPress API to check token
 	try {
 		const { data } = await fetchHookData(
@@ -90,7 +85,6 @@ export async function revalidateHandler(req: NextApiRequest, res: NextApiRespons
 		await res.revalidate(pathToRevalidate);
 		return res.status(200).json({ message: 'success', path: pathToRevalidate });
 	} catch (err) {
-		console.error(err);
 		let errorMessage = 'Error verifying the token';
 		if (err instanceof Error) {
 			errorMessage = err.message;
