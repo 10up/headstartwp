1774c80ad3a65a00e31a21cdb36657a92855a0d3
Nícholas André <nicholas.andre@10up.com>
2023-06-14 17:09:21 -0300

    feat: vip custom image loader

--- packages/next/src/components/ImageComponent.tsx
+++ packages/next/src/components/ImageComponent.tsx
@@ -1,6 +1,79 @@
-import Image from 'next/image';
+import Image, { ImageLoaderProps } from 'next/image';
 
 import { ImageBlockProps } from '@headstartwp/core/react';
+
+/**
+ * Given a width and an aspect ratio, calculate the resulting height. If either the
+ * calculation fails or any of the parameters are missing, it won't return anything.
+ *
+ * @param {?number} width       Given width
+ * @param {?string} aspectRatio Aspect Ratio in the form of `16:9`
+ */
+function calculateHeightFromWidth(width?: number, aspectRatio?: string) {
+	let result;
+
+	if (!width || !aspectRatio) {
+		return undefined;
+	}
+
+	try {
+		const [w, h] = aspectRatio.split(':').map(Number);
+		const ratio = h / w;
+		result = Math.round(width * ratio);
+	} catch (error) {
+		// Error silently
+	}
+
+	return result;
+}
+
+export type VIPCustomImageLoader = ImageLoaderProps & { aspectRatio?: string };
+
+/**
+ * Custom Image loader for WordPress VIP leveraging Photon's API. This can't be made
+ * a global loader because of the local loader used on the custom image component.
+ *
+ * @param {object}  props 			  The loader props
+ * @param {?number} props.quality     Number of quality where 0 is minimum and 100 is maximum. 75 if not passed.
+ * @param {string}  props.src         Image source
+ * @param {number}  props.width       Width for the image
+ * @param {?string} props.aspectRatio Aspect Ratio in the form of `16:9`
+ */
+export function VIPImageLoader({ quality, src, width, aspectRatio }: VIPCustomImageLoader) {
+	if (src.includes('/wp-content/uploads')) {
+		const url = new URL(src);
+		const height = calculateHeightFromWidth(width, aspectRatio);
+
+		if (width) {
+			if (url.searchParams.has('w')) {
+				// If it has height but we're overriding, don't bother
+				if (url.searchParams.has('h') && !height) {
+					const originalHeight = Number(url.searchParams.get('h'));
+					const originalWidth = Number(url.searchParams.get('w'));
+					const newHeight = Math.round((originalHeight / originalWidth) * width);
+					url.searchParams.set('h', `${newHeight}`);
+				}
+			}
+
+			url.searchParams.set('w', `${width}`);
+		}
+
+		if (height) {
+			url.searchParams.set('h', `${height}`);
+			url.searchParams.set('crop', '1');
+		}
+
+		url.searchParams.set('q', `${quality || 75}`);
+
+		return url.toString();
+	}
+
+	return src;
+}
+
+export interface ImageProps extends ImageBlockProps {
+	loader?: (props: VIPCustomImageLoader | ImageLoaderProps) => string;
+}
 
 /**
  * An implementation of the ImageBlock (core/image).
@@ -22,15 +95,24 @@ import { ImageBlockProps } from '@headstartwp/core/react';
  *
  * @category React Components
  */
-export function ImageComponent({ src, alt, width, height, children, style }: ImageBlockProps) {
+export function ImageComponent({ src, alt, width, height, children, style, loader }: ImageProps) {
 	if (!src) {
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		return <>{children}</>;
 	}
 
 	if (!width || !height) {
-		<Image src={src} alt={alt || ''} fill style={style} />;
+		<Image src={src} alt={alt || ''} fill style={style} loader={loader} />;
 	}
 
-	return <Image src={src} alt={alt || ''} width={width} height={height} style={style} />;
+	return (
+		<Image
+			src={src}
+			alt={alt || ''}
+			width={width}
+			height={height}
+			style={style}
+			loader={loader}
+		/>
+	);
 }
---
+++ packages/next/src/components/__tests__/ImageComponent.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ImageComponent, VIPImageLoader } from '../ImageComponent';
+
+describe('VIPImageLoader', () => {
+	it('only works with WP images', () => {
+		expect(
+			VIPImageLoader({ src: 'http://wp.com/wp-content/uploads/2022/20/img.jpg', width: 100 }),
+		).toBe('http://wp.com/wp-content/uploads/2022/20/img.jpg?w=100&q=75');
+
+		expect(VIPImageLoader({ src: 'http://wp.com/img.jpg', width: 100 })).toBe(
+			'http://wp.com/img.jpg',
+		);
+	});
+
+	it('maitains the aspect ratio', () => {
+		expect(
+			VIPImageLoader({
+				src: 'http://wp.com/wp-content/uploads/2022/20/img.jpg',
+				width: 100,
+				aspectRatio: '16:9',
+			}),
+		).toBe('http://wp.com/wp-content/uploads/2022/20/img.jpg?w=100&h=56&crop=1&q=75');
+	});
+});
+
+describe('ImageComponent', () => {
+	it('renders', () => {
+		const src = 'http://wp.com/wp-content/uploads/2022/20/img.jpg';
+		const alt = 'image';
+
+		render(<ImageComponent name="core/image" src={src} alt={alt} width={100} height={100} />);
+
+		const img = screen.getByAltText(alt);
+		expect(img.getAttribute('src')).toBe(
+			'/_next/image?url=http%3A%2F%2Fwp.com%2Fwp-content%2Fuploads%2F2022%2F20%2Fimg.jpg&w=256&q=75',
+		);
+	});
+
+	it('renders with a cusotm loader', () => {
+		const src = 'http://wp.com/wp-content/uploads/2022/20/img.jpg';
+		const alt = 'image';
+
+		render(
+			<ImageComponent
+				name="core/image"
+				src={src}
+				alt={alt}
+				width={100}
+				height={100}
+				loader={() => 'https://my-custom-image-loader.com'}
+			/>,
+		);
+
+		const img = screen.getByAltText(alt);
+
+		expect(img.getAttribute('src')).toBe('https://my-custom-image-loader.com');
+	});
+
+	it('renders with the vip image loader', () => {
+		const src = 'http://wp.com/wp-content/uploads/2022/20/img.jpg';
+		const alt = 'image';
+
+		render(
+			<ImageComponent
+				name="core/image"
+				src={src}
+				alt={alt}
+				width={100}
+				height={100}
+				loader={VIPImageLoader}
+			/>,
+		);
+
+		const img = screen.getByAltText(alt);
+
+		expect(img.getAttribute('src')).toBe(
+			'http://wp.com/wp-content/uploads/2022/20/img.jpg?w=256&q=75',
+		);
+	});
+});
