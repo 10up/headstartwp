34c6284cd02cf0595e8f8d75b14f0d845b8606a1
Clayton Collie <clayton.collie@gmail.com>
2024-02-15 13:54:44 -0600

    Fix constant reference in config

--- wp/headless-wp/rector.php
+++ wp/headless-wp/rector.php
@@ -8,7 +8,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
+use Rector\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
