cb98f07b7b87ae59f8b6d91b0c3b9eda1d1024c8
Nícholas André <nicholas.andre@10up.com>
2024-03-02 14:19:56 -0300

    feat: forcing drafts to have permalinks

--- wp/headless-wp/includes/classes/Preview/PreviewLink.php
+++ wp/headless-wp/includes/classes/Preview/PreviewLink.php
@@ -17,6 +17,41 @@ class PreviewLink {
 	 */
 	public function register() {
 		add_filter( 'template_include', [ $this, 'handle_preview' ], 20 );
+		add_filter( 'get_post_status', [ $this, 'force_drafts_to_have_permalinks' ], 10, 2 );
+	}
+
+	/**
+	 * Force drafts to have permalinks
+	 *
+	 * @param string   $post_status The post's permalink.
+	 * @param \WP_Post $post        The post in question.
+	 *
+	 * @return string
+	 */
+	public function force_drafts_to_have_permalinks( string $post_status, \WP_Post $post ) : string {
+		// If this isn't a rest request do nothing
+		if ( ! ( defined( 'REST_REQUEST' ) && REST_REQUEST ) ) {
+			return $post_status;
+		}
+
+		if ( 'draft' !== $post_status ) {
+			return $post_status;
+		}
+
+		try {
+			$payload = PreviewToken::get_payload_from_token();
+
+			if ( ! $payload ) {
+				return $post_status;
+			}
+
+			if ( 'preview' === $payload->type && $post->ID === $payload->post_id ) {
+				return 'publish';
+			}
+			return $post_status;
+		} catch ( \Exception $e ) {
+			return $post_status;
+		}
 	}
 
 	/**
