6ffeea337ab10b6b009e576186725a2d91cd796e
Nícholas André <nicholas.andre@10up.com>
2024-04-02 17:33:41 -0300

    refactor: remove redis test code from default config

--- projects/wp-nextjs/headstartwp.config.server.js
+++ projects/wp-nextjs/headstartwp.config.server.js
@@ -1,7 +1,4 @@
-const { getRedisClient } = require('@10up/next-redis-cache-provider');
 const baseConfig = require('./headstartwp.config.client');
-
-const redisClient = getRedisClient();
 
 /**
  * Config
@@ -11,22 +8,6 @@ const redisClient = getRedisClient();
 module.exports = {
 	...baseConfig,
 	cache: {
-		enabled: ({ fetchStrategy }) => {
-			// cache app endpoints in-memory by default
-			return fetchStrategy.getEndpoint() === '/wp-json/headless-wp/v1/app';
-		},
-
-		/**
-		 * @type {import('@headstartwp/core').FetchStrategyCacheHandler}
-		 */
-		cacheHandler: {
-			async set(key, data, ttl) {
-				return redisClient.set(key, JSON.stringify(data), 'EX', ttl);
-			},
-			async get(key) {
-				const data = await redisClient.get(key);
-				return JSON.parse(data);
-			},
-		},
+		enabled: false,
 	},
 };
