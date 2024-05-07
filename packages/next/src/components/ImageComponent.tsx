1c413938be7d52221d1a292dc95b38c73e989001
Nícholas Oliveira <nicholas.andre@10up.com>
2024-05-07 16:03:46 -0300

    fix: address next.js inconsistencies

--- packages/next/src/components/ImageComponent.tsx
+++ packages/next/src/components/ImageComponent.tsx
@@ -1,5 +1,5 @@
-import NextImageComponent from 'next/image';
-import type { ImageLoaderProps } from 'next/image';
+import NextImageComponent from 'next/image.js';
+import type { ImageLoaderProps } from 'next/image.js';
 import { ImageBlockProps } from '@headstartwp/core/react';
 
 // See error in https://github.com/vercel/next.js/issues/54777
