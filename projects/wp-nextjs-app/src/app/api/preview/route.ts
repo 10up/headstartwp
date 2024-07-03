631dbdc555c311ea4300e3fcbe3cb540be8d041f
Nícholas Oliveira <nicholas.andre@10up.com>
2024-07-03 20:30:59 -0300

    fix: config loading for route handlers

--- projects/wp-nextjs-app/src/app/api/preview/route.ts
+++ projects/wp-nextjs-app/src/app/api/preview/route.ts
@@ -1,9 +1,5 @@
 import { previewRouteHandler } from '@headstartwp/next/app';
 import type { NextRequest } from 'next/server';
-import { setHeadlessConfig } from '@headstartwp/core';
-import config from '../../../../headstartwp.config';
-
-setHeadlessConfig(config);
 
 export async function GET(request: NextRequest) {
 	// @ts-expect-error
