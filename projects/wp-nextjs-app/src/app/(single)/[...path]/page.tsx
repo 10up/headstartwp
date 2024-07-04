2a17cf170fb5549a21ed01e93b4dc3ee1dff7146
Nícholas Oliveira <nicholas.andre@10up.com>
2024-07-03 21:01:10 -0300

    feat: update single route to render post content

--- projects/wp-nextjs-app/src/app/(single)/[...path]/page.tsx
+++ projects/wp-nextjs-app/src/app/(single)/[...path]/page.tsx
@@ -1,4 +1,4 @@
-import { HtmlDecoder } from '@headstartwp/core/react';
+import { BlocksRenderer, HtmlDecoder } from '@headstartwp/core/react';
 import { HeadstartWPRoute, queryPost } from '@headstartwp/next/app';
 
 const Single = async ({ params }: HeadstartWPRoute) => {
@@ -11,6 +11,8 @@ const Single = async ({ params }: HeadstartWPRoute) => {
 			<h1>
 				<HtmlDecoder html={data.post.title.rendered ?? ''} />
 			</h1>
+
+			<BlocksRenderer html={data.post.content.rendered ?? ''} />
 		</article>
 	);
 };
