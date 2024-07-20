a73b59933ce68cafbfdd345699e4021fabddecdf
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-07-20 21:59:08 +0530

    feat: add `menu-item-has-children` class if menu item has children

--- packages/core/src/react/components/Menu.tsx
+++ packages/core/src/react/components/Menu.tsx
@@ -71,10 +71,19 @@ export const MenuItems = ({
 					backendUrl: settings.sourceUrl || '',
 				});
 				const shouldLink = item.children.length === 0 || topLevelItemsClickable;
-				const className = `menu-item-depth-${depth}`;
+				const classNames = [`menu-item-depth-${depth}`];
+
+				if (item.children.length) {
+					classNames.push('menu-item-has-children');
+				}
 
 				return (
-					<ItemWrapper key={item.ID} className={className} depth={depth} item={item}>
+					<ItemWrapper
+						key={item.ID}
+						className={classNames.join(' ')}
+						depth={depth}
+						item={item}
+					>
 						{shouldLink ? (
 							<LinkWrapper href={link} depth={depth}>
 								{item.title}
