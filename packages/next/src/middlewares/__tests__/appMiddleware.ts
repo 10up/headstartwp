376dbcadb9c1b7741756ccdae5ece1c45cb70c62
Jacob Dubail <jacob.dubail@10up.com>
2024-12-12 13:04:21 -0800

    fix: test label typo

--- packages/next/src/middlewares/__tests__/appMiddleware.ts
+++ packages/next/src/middlewares/__tests__/appMiddleware.ts
@@ -389,7 +389,7 @@ describe('appMiddleware', () => {
 		res = await AppMiddleware(req, { appRouter: true });
 		expect(res.headers.get('x-middleware-rewrite')).toBe('http://test.com/en/post-name');
 
-		// add default locale with a query string, expecting this to fail
+		// add default locale with a query string
 		req = new NextRequest('http://test.com/post-name?s=query', {
 			method: 'GET',
 		});
