cff783614e579893ad39241b3e7345bfcb849e43
Nícholas André <nicholas.andre@10up.com>
2025-04-09 18:18:12 -0300

    fix: only flush app settings when updating app settings

--- wp/headless-wp/includes/classes/API/AppEndpoint.php
+++ wp/headless-wp/includes/classes/API/AppEndpoint.php
@@ -223,7 +223,7 @@ class AppEndpoint {
 	 * @return void
 	 */
 	public function maybe_invalidate_cache( $option_name ) {
-		if ( self::$cache_key_store !== $option_name ) {
+		if ( self::$cache_key_store === $option_name ) {
 			$this->invalidate_cache();
 		}
 	}
