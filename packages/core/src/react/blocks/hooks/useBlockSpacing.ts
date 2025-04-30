ee5be48adf5531fcd36ff7ebb3a1b0f1bdcd8602
Nícholas André <nicholas.andre@10up.com>
2025-04-30 09:18:05 -0300

    fix: blockGap in useBlockSpacing (#906)

--- packages/core/src/react/blocks/hooks/useBlockSpacing.ts
+++ packages/core/src/react/blocks/hooks/useBlockSpacing.ts
@@ -10,6 +10,7 @@ interface BlockSpacingAttributes extends IBlockAttributes {
 		spacing: {
 			padding: Spacing['padding'];
 			margin: Spacing['margin'];
+			blockGap: Spacing['blockGap'];
 		};
 	};
 }
@@ -27,12 +28,19 @@ export function useBlockSpacing(node: Element): Spacing {
 	const supportsPadding = !!useThemeSetting('spacing.padding', name);
 	const styles = useThemeStyles();
 
+	let blockGap: Spacing['blockGap'] = '';
+	if (supportsBlockGap && attributes?.styleConfig?.spacing?.blockGap) {
+		blockGap = attributes?.styleConfig?.spacing?.blockGap;
+	} else if (supportsBlockGap && styles?.spacing?.blockGap) {
+		blockGap = styles?.spacing?.blockGap;
+	}
+
 	return {
 		padding: attributes?.styleConfig?.spacing?.padding,
 		margin: attributes?.styleConfig?.spacing?.margin,
 		supportsMargin,
 		supportsPadding,
 		supportsBlockGap,
-		blockGap: supportsBlockGap && styles?.spacing?.blockGap ? styles?.spacing?.blockGap : '',
+		blockGap,
 	};
 }
