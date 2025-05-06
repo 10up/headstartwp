4b342a5549bbfd0102f892b3d1773e82c5740725
Nícholas Oliveira <nicholas.andre@10up.com>
2025-05-06 09:12:20 -0300

    fix: formatting

--- wp/headless-wp/tests/php/tests/TestGutenbergIntegration.php
+++ wp/headless-wp/tests/php/tests/TestGutenbergIntegration.php
@@ -238,7 +238,7 @@ RESULT;
 		$this->assertEquals( $doc->get_attribute( 'height' ), 235 );
 
 		// simulate an external image
-		$block          = $this->core_render_block_from_markup( "<!-- wp:image {} --> <figure class=\"wp-block-image\"><img src=\"https://example.com/image.png\" alt=\"\"/></figure> <!-- /wp:image -->" );
+		$block          = $this->core_render_block_from_markup( '<!-- wp:image {} --> <figure class="wp-block-image"><img src="https://example.com/image.png" alt=""/></figure> <!-- /wp:image -->' );
 		$enhanced_block = $this->parser->ensure_image_has_dimensions( $block['html'], $block['parsed_block'] );
 
 		$doc = new WP_HTML_Tag_Processor( $enhanced_block );
