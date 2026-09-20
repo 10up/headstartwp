be0d3ef75d4ac1a501e05443307f92ece7278495
Preston Davies <preston.davies@get10up.com>
2026-09-19 21:46:28 -0400

    chore: revert formatter drift, add changesets, harden version script

--- packages/core/src/data/strategies/SinglePostFetchStrategy.ts
+++ packages/core/src/data/strategies/SinglePostFetchStrategy.ts
@@ -1,67 +1,67 @@
 import {
-  getCustomPostType,
-  ConfigError,
-  EndpointError,
-  removeSourceUrl,
-  NotFoundError,
-  getSiteBySourceUrl,
-  addQueryArgs,
-} from "../../utils";
-import { PostEntity } from "../types";
-import { postMatchers } from "../utils/matchers";
-import { parsePath } from "../utils/parsePath";
+	getCustomPostType,
+	ConfigError,
+	EndpointError,
+	removeSourceUrl,
+	NotFoundError,
+	getSiteBySourceUrl,
+	addQueryArgs,
+} from '../../utils';
+import { PostEntity } from '../types';
+import { postMatchers } from '../utils/matchers';
+import { parsePath } from '../utils/parsePath';
 import {
-  AbstractFetchStrategy,
-  EndpointParams,
-  FetchOptions,
-  FetchResponse,
-  FilterDataOptions,
-} from "./AbstractFetchStrategy";
-import { endpoints, removeFieldsFromPostRelatedData } from "../utils";
-import { removeFields } from "../utils/dataFilter";
-import { apiGet } from "../api";
+	AbstractFetchStrategy,
+	EndpointParams,
+	FetchOptions,
+	FetchResponse,
+	FilterDataOptions,
+} from './AbstractFetchStrategy';
+import { endpoints, removeFieldsFromPostRelatedData } from '../utils';
+import { removeFields } from '../utils/dataFilter';
+import { apiGet } from '../api';
 
 /**
  * The EndpointParams supported by the {@link SinglePostFetchStrategy}
  */
 export interface PostParams extends EndpointParams {
-  /**
-   * The slug of the post to fetch
-   */
-  slug?: string;
+	/**
+	 * The slug of the post to fetch
+	 */
+	slug?: string;
 
-  /**
-   * Post Types where we should look for
-   *
-   * If multiple post types are specified
-   * multiple requests will be issued to each post type until a matching post is found
-   */
-  postType?: string | string[];
+	/**
+	 * Post Types where we should look for
+	 *
+	 * If multiple post types are specified
+	 * multiple requests will be issued to each post type until a matching post is found
+	 */
+	postType?: string | string[];
 
-  /**
-   * Fetch post by id
-   */
-  id?: number;
+	/**
+	 * Fetch post by id
+	 */
+	id?: number;
 
-  /**
-   * If set will fetch the latest post revision
-   */
-  revision?: boolean;
+	/**
+	 * If set will fetch the latest post revision
+	 */
+	revision?: boolean;
 
-  /**
-   * The authToken, required to fetch revisions or non-published posts
-   */
-  authToken?: string;
+	/**
+	 * The authToken, required to fetch revisions or non-published posts
+	 */
+	authToken?: string;
 
-  /**
-   * Whether post.link should be checked against current path
-   */
-  matchCurrentPath?: boolean;
+	/**
+	 * Whether post.link should be checked against current path
+	 */
+	matchCurrentPath?: boolean;
 
-  /**
-   * If set, this is the path that will be checked if `slug` is set or `matchCurrentPath` is set to true.
-   */
-  fullPath?: string;
+	/**
+	 * If set, this is the path that will be checked if `slug` is set or `matchCurrentPath` is set to true.
+	 */
+	fullPath?: string;
 }
 
 /**
@@ -78,296 +78,296 @@ export interface PostParams extends EndpointParams {
  * @category Data Fetching
  */
 export class SinglePostFetchStrategy<
-  T extends PostEntity = PostEntity,
-  P extends PostParams = PostParams,
+	T extends PostEntity = PostEntity,
+	P extends PostParams = PostParams,
 > extends AbstractFetchStrategy<T[], P, T> {
-  postType: string = "post";
+	postType: string = 'post';
 
-  revision?: PostEntity;
+	revision?: PostEntity;
 
-  path: string = "";
+	path: string = '';
 
-  locale: string = "";
+	locale: string = '';
 
-  shouldCheckCurrentPathAgainstPostLink: boolean = true;
+	shouldCheckCurrentPathAgainstPostLink: boolean = true;
 
-  optimizeYoastPayload: boolean = false;
+	optimizeYoastPayload: boolean = false;
 
-  getDefaultEndpoint(): string {
-    return endpoints.posts;
-  }
+	getDefaultEndpoint(): string {
+		return endpoints.posts;
+	}
 
-  getDefaultParams(): Partial<P> {
-    return { _embed: true, ...super.getDefaultParams() } as P;
-  }
+	getDefaultParams(): Partial<P> {
+		return { _embed: true, ...super.getDefaultParams() } as P;
+	}
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  getParamsFromURL(path: string, nonUrlParams: Partial<P> = {}): Partial<P> {
-    const config = getSiteBySourceUrl(this.baseURL);
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	getParamsFromURL(path: string, nonUrlParams: Partial<P> = {}): Partial<P> {
+		const config = getSiteBySourceUrl(this.baseURL);
 
-    // this is required for post path mapping
-    this.locale =
-      config.integrations?.polylang?.enable && nonUrlParams.lang ? nonUrlParams.lang : "";
+		// this is required for post path mapping
+		this.locale =
+			config.integrations?.polylang?.enable && nonUrlParams.lang ? nonUrlParams.lang : '';
 
-    this.path = nonUrlParams.fullPath ?? path;
+		this.path = nonUrlParams.fullPath ?? path;
 
-    this.optimizeYoastPayload = !!config.integrations?.yoastSEO?.optimizeYoastPayload;
+		this.optimizeYoastPayload = !!config.integrations?.yoastSEO?.optimizeYoastPayload;
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { year, day, month, ...params } = parsePath(postMatchers, path);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { year, day, month, ...params } = parsePath(postMatchers, path);
 
-    // if slug is passed, it is being manually overridden then don't check current path
-    this.shouldCheckCurrentPathAgainstPostLink =
-      nonUrlParams.matchCurrentPath ?? typeof nonUrlParams.slug === "undefined";
+		// if slug is passed, it is being manually overridden then don't check current path
+		this.shouldCheckCurrentPathAgainstPostLink =
+			nonUrlParams.matchCurrentPath ?? typeof nonUrlParams.slug === 'undefined';
 
-    // TODO: figure typings for this
-    return params as Partial<P>;
-  }
+		// TODO: figure typings for this
+		return params as Partial<P>;
+	}
 
-  /**
-   * Handlers post types, revisions and fetching by id
-   *
-   * @param params The params to build the endpoint url
-   */
-  buildEndpointURL(params: P) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { id, authToken, revision, postType, matchCurrentPath, fullPath, ...endpointParams } =
-      params;
+	/**
+	 * Handlers post types, revisions and fetching by id
+	 *
+	 * @param params The params to build the endpoint url
+	 */
+	buildEndpointURL(params: P) {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { id, authToken, revision, postType, matchCurrentPath, fullPath, ...endpointParams } =
+			params;
 
-    if (params.postType) {
-      // if postType is a array of slugs, start off with the first post type
-      const postTypeSlug = Array.isArray(params.postType) ? params.postType[0] : params.postType;
+		if (params.postType) {
+			// if postType is a array of slugs, start off with the first post type
+			const postTypeSlug = Array.isArray(params.postType) ? params.postType[0] : params.postType;
 
-      const postType = getCustomPostType(postTypeSlug, this.baseURL);
+			const postType = getCustomPostType(postTypeSlug, this.baseURL);
 
-      if (!postType) {
-        throw new ConfigError("Unknown post type, did you forget to add it to headless.config.js?");
-      }
-      this.postType = postType.slug;
-      this.setEndpoint(postType.endpoint);
-    }
+			if (!postType) {
+				throw new ConfigError('Unknown post type, did you forget to add it to headless.config.js?');
+			}
+			this.postType = postType.slug;
+			this.setEndpoint(postType.endpoint);
+		}
 
-    if (id) {
-      this.setEndpoint(`${this.getEndpoint()}/${id}`);
-      if (endpointParams.slug) {
-        delete endpointParams.slug;
-      }
-    }
+		if (id) {
+			this.setEndpoint(`${this.getEndpoint()}/${id}`);
+			if (endpointParams.slug) {
+				delete endpointParams.slug;
+			}
+		}
 
-    return super.buildEndpointURL(endpointParams as P);
-  }
+		return super.buildEndpointURL(endpointParams as P);
+	}
 
-  /**
-   * Returns only the post that matches the current path
-   *
-   * @param result
-   * @param params
-   * @returns
-   */
-  getPostThatMatchesCurrentPath(result: T[], params: Partial<P>): T | undefined {
-    const currentPath = decodeURIComponent(this.path).replace(/\/?$/, "/");
+	/**
+	 * Returns only the post that matches the current path
+	 *
+	 * @param result
+	 * @param params
+	 * @returns
+	 */
+	getPostThatMatchesCurrentPath(result: T[], params: Partial<P>): T | undefined {
+		const currentPath = decodeURIComponent(this.path).replace(/\/?$/, '/');
 
-    return result.find((post) => {
-      const postPath = decodeURIComponent(
-        removeSourceUrl({
-          link: post.link,
-          backendUrl: this.baseURL,
-        }),
-      )?.replace(/\/?$/, "/");
+		return result.find((post) => {
+			const postPath = decodeURIComponent(
+				removeSourceUrl({
+					link: post.link,
+					backendUrl: this.baseURL,
+				}),
+			)?.replace(/\/?$/, '/');
 
-      if (params.postType && params.postType.length > 0) {
-        const expectedPostTypes = Array.isArray(params.postType)
-          ? params.postType
-          : [params.postType];
-        const postType = post.type ?? "";
+			if (params.postType && params.postType.length > 0) {
+				const expectedPostTypes = Array.isArray(params.postType)
+					? params.postType
+					: [params.postType];
+				const postType = post.type ?? '';
 
-        if (expectedPostTypes.includes(postType)) {
-          const postTypeObject = getCustomPostType(postType, this.baseURL);
+				if (expectedPostTypes.includes(postType)) {
+					const postTypeObject = getCustomPostType(postType, this.baseURL);
 
-          if (params.fullPath) {
-            const normalizedFullPath = params.fullPath.replace(/\/?$/, "/");
-            return (
-              postPath === normalizedFullPath || postPath === `/${this.locale}${normalizedFullPath}`
-            );
-          }
+					if (params.fullPath) {
+						const normalizedFullPath = params.fullPath.replace(/\/?$/, '/');
+						return (
+							postPath === normalizedFullPath || postPath === `/${this.locale}${normalizedFullPath}`
+						);
+					}
 
-          const singlePrefix = postTypeObject?.single?.replace(/\/?$/, "") ?? "";
+					const singlePrefix = postTypeObject?.single?.replace(/\/?$/, '') ?? '';
 
-          return (
-            postPath === `${singlePrefix}${currentPath}` ||
-            postPath === `/${this.locale}${singlePrefix}${currentPath}`
-          );
-        }
-      }
+					return (
+						postPath === `${singlePrefix}${currentPath}` ||
+						postPath === `/${this.locale}${singlePrefix}${currentPath}`
+					);
+				}
+			}
 
-      const postPostTypeObject = getCustomPostType("post", this.baseURL);
-      const postSinglePrefix = postPostTypeObject?.single?.replace(/\/?$/, "") ?? "";
+			const postPostTypeObject = getCustomPostType('post', this.baseURL);
+			const postSinglePrefix = postPostTypeObject?.single?.replace(/\/?$/, '') ?? '';
 
-      return (
-        postPath === `${postSinglePrefix}${currentPath}` ||
-        postPath === `/${this.locale}${postSinglePrefix}${currentPath}`
-      );
-    });
-  }
+			return (
+				postPath === `${postSinglePrefix}${currentPath}` ||
+				postPath === `/${this.locale}${postSinglePrefix}${currentPath}`
+			);
+		});
+	}
 
-  /**
-   * Prepares the post response
-   *
-   * @param response
-   * @param params
-   * @returns
-   */
-  prepareResponse(response: FetchResponse<T[] | T>, params: Partial<P>): FetchResponse<T> {
-    const { result } = response;
+	/**
+	 * Prepares the post response
+	 *
+	 * @param response
+	 * @param params
+	 * @returns
+	 */
+	prepareResponse(response: FetchResponse<T[] | T>, params: Partial<P>): FetchResponse<T> {
+		const { result } = response;
 
-    if (
-      typeof this.revision !== "undefined" &&
-      typeof params.id !== "undefined" &&
-      !Array.isArray(response.result)
-    ) {
-      const revisionContent = {
-        content: { ...this.revision.content },
-        excerpt: { ...this.revision.excerpt },
-        title: { ...this.revision.title },
-      };
-      return {
-        ...response,
-        result: { ...response.result, ...revisionContent },
-      };
-    }
+		if (
+			typeof this.revision !== 'undefined' &&
+			typeof params.id !== 'undefined' &&
+			!Array.isArray(response.result)
+		) {
+			const revisionContent = {
+				content: { ...this.revision.content },
+				excerpt: { ...this.revision.excerpt },
+				title: { ...this.revision.title },
+			};
+			return {
+				...response,
+				result: { ...response.result, ...revisionContent },
+			};
+		}
 
-    if (params.id && !Array.isArray(result)) {
-      this.setEndpoint(this.getDefaultEndpoint());
-      return {
-        ...response,
-        result,
-      };
-    }
+		if (params.id && !Array.isArray(result)) {
+			this.setEndpoint(this.getDefaultEndpoint());
+			return {
+				...response,
+				result,
+			};
+		}
 
-    // if result is an array, prioritize the result where the
-    // link property matches with the current route
-    if (Array.isArray(result)) {
-      if (result.length === 0) {
-        return {
-          ...response,
-          result: {} as T,
-        };
-      }
-      const shouldCheckCurrentPath =
-        this.path.length > 0 && this.path !== "/" && this.shouldCheckCurrentPathAgainstPostLink;
+		// if result is an array, prioritize the result where the
+		// link property matches with the current route
+		if (Array.isArray(result)) {
+			if (result.length === 0) {
+				return {
+					...response,
+					result: {} as T,
+				};
+			}
+			const shouldCheckCurrentPath =
+				this.path.length > 0 && this.path !== '/' && this.shouldCheckCurrentPathAgainstPostLink;
 
-      const post = shouldCheckCurrentPath
-        ? this.getPostThatMatchesCurrentPath(result, params)
-        : result[0];
+			const post = shouldCheckCurrentPath
+				? this.getPostThatMatchesCurrentPath(result, params)
+				: result[0];
 
-      if (!post) {
-        throw new NotFoundError(
-          `Post #${result[0].id} - "${result[0].link}" was found but did not match current path: "${this.path}""`,
-        );
-      }
+			if (!post) {
+				throw new NotFoundError(
+					`Post #${result[0].id} - "${result[0].link}" was found but did not match current path: "${this.path}""`,
+				);
+			}
 
-      return {
-        ...response,
-        result: post,
-      };
-    }
+			return {
+				...response,
+				result: post,
+			};
+		}
 
-    return {
-      ...response,
-      result,
-    };
-  }
+		return {
+			...response,
+			result,
+		};
+	}
 
-  /**
-   * Handles fetching by multiple post types, authToken and revisions
-   *
-   * @param url The url to fetch
-   * @param params The params to build the endpoint url
-   * @param options FetchOptions
-   */
-  async fetcher(url: string, params: P, options: Partial<FetchOptions> = {}) {
-    const { burstCache = false } = options;
-    let finalUrl = url;
+	/**
+	 * Handles fetching by multiple post types, authToken and revisions
+	 *
+	 * @param url The url to fetch
+	 * @param params The params to build the endpoint url
+	 * @param options FetchOptions
+	 */
+	async fetcher(url: string, params: P, options: Partial<FetchOptions> = {}) {
+		const { burstCache = false } = options;
+		let finalUrl = url;
 
-    if (params.authToken) {
-      options.previewToken = params.authToken;
-    }
+		if (params.authToken) {
+			options.previewToken = params.authToken;
+		}
 
-    const authHeader = this.getPreviewAuthHeader(options);
+		const authHeader = this.getPreviewAuthHeader(options);
 
-    let error;
-    if (params.revision && params.id) {
-      try {
-        const response = await apiGet(
-          `${this.baseURL}${this.getEndpoint()}/revisions?per_page=1`,
-          {
-            headers: {
-              [this.getPreviewHeaderName(options)]: authHeader,
-            },
-            cache: "no-store",
-          },
-          burstCache,
-        );
+		let error;
+		if (params.revision && params.id) {
+			try {
+				const response = await apiGet(
+					`${this.baseURL}${this.getEndpoint()}/revisions?per_page=1`,
+					{
+						headers: {
+							[this.getPreviewHeaderName(options)]: authHeader,
+						},
+						cache: 'no-store',
+					},
+					burstCache,
+				);
 
-        if (Array.isArray(response.json) && response.json.length > 0) {
-          this.revision = response.json[0];
-        }
-      } catch (e) {
-        throw new EndpointError("Unable to fetch latest revision");
-      }
-    }
+				if (Array.isArray(response.json) && response.json.length > 0) {
+					this.revision = response.json[0];
+				}
+			} catch (e) {
+				throw new EndpointError('Unable to fetch latest revision');
+			}
+		}
 
-    try {
-      if (this.optimizeYoastPayload) {
-        finalUrl = addQueryArgs(finalUrl, {
-          optimizeYoastPayload: true,
-        });
-      }
+		try {
+			if (this.optimizeYoastPayload) {
+				finalUrl = addQueryArgs(finalUrl, {
+					optimizeYoastPayload: true,
+				});
+			}
 
-      const result = await super.fetcher(finalUrl, params, options);
+			const result = await super.fetcher(finalUrl, params, options);
 
-      return result;
-    } catch (e) {
-      error = e;
-    }
+			return result;
+		} catch (e) {
+			error = e;
+		}
 
-    // should throw error if didn't find anything and params.postType is not an array.
-    if (!Array.isArray(params.postType)) {
-      throw error;
-    }
+		// should throw error if didn't find anything and params.postType is not an array.
+		if (!Array.isArray(params.postType)) {
+			throw error;
+		}
 
-    // skip first post type as it has already been fetched
-    const [, ...postTypes] = params.postType;
+		// skip first post type as it has already been fetched
+		const [, ...postTypes] = params.postType;
 
-    for await (const postType of postTypes) {
-      try {
-        this.postType = postType;
-        const newParams = { ...params, postType };
-        const endpointUrl = this.buildEndpointURL({ ...newParams, postType });
+		for await (const postType of postTypes) {
+			try {
+				this.postType = postType;
+				const newParams = { ...params, postType };
+				const endpointUrl = this.buildEndpointURL({ ...newParams, postType });
 
-        const result = await super.fetcher(endpointUrl, newParams, options);
-        return result;
-      } catch (e) {
-        error = e;
-      }
-    }
+				const result = await super.fetcher(endpointUrl, newParams, options);
+				return result;
+			} catch (e) {
+				error = e;
+			}
+		}
 
-    // if gets to the this point then nothing was found then thrown
-    throw error;
-  }
+		// if gets to the this point then nothing was found then thrown
+		throw error;
+	}
 
-  filterData(data: FetchResponse<PostEntity>, filterOptions?: FilterDataOptions<PostEntity>) {
-    if (filterOptions) {
-      return this.filterData(data, filterOptions);
-    }
+	filterData(data: FetchResponse<PostEntity>, filterOptions?: FilterDataOptions<PostEntity>) {
+		if (filterOptions) {
+			return this.filterData(data, filterOptions);
+		}
 
-    const fieldsToRemove = ["_links"];
+		const fieldsToRemove = ['_links'];
 
-    const post = removeFields(fieldsToRemove, data.result) as PostEntity;
+		const post = removeFields(fieldsToRemove, data.result) as PostEntity;
 
-    return {
-      ...data,
-      result: removeFieldsFromPostRelatedData(fieldsToRemove, post),
-    };
-  }
+		return {
+			...data,
+			result: removeFieldsFromPostRelatedData(fieldsToRemove, post),
+		};
+	}
 }
